29ba8ca0f7e1e3649f215e9dccfde154c3dec171
RyanMcklain <piro27@gmail.com>
2022-06-27 07:36:12 +0300

    add basic velocity to player

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -3,19 +3,41 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class Player : MonoBehaviour {
-    private float update;
+    public Transform groundCheckTransform;
+    private bool isJumpKeyPressed;
+    private float horizontalInput;
+    private Rigidbody rigidPlayer;
+    private LayerMask playerMask;
 
     // Start is called before the first frame update
     void Start() {
-        Debug.Log("Start1");
+        Debug.Log("Start!");
+        rigidPlayer = GetComponent<Rigidbody>();
     }
 
     // Update is called once per frame
     void Update() {
-        update += Time.deltaTime;
-        if (update > 1.0f) {
-            update = 0.0f;
-            Debug.Log("Update");
+        if (Input.GetKeyDown(KeyCode.Space)) {
+            isJumpKeyPressed = true;
+        }
+
+        horizontalInput = Input.GetAxis("Horizontal");
+    }
+
+    // once per engine, 100 per seconds in the current unity engine
+    private void FixedUpdate() {
+        rigidPlayer.velocity = new Vector3(horizontalInput * 8, rigidPlayer.velocity.y, 0);
+
+        // Including thyself
+        if (Physics.OverlapSphere(groundCheckTransform.position, 0.1f, playerMask).Length == 0) {
+            return;
+        }
+
+
+        if (isJumpKeyPressed)
+        {
+            rigidPlayer.AddForce(Vector3.up * 30, ForceMode.Impulse);
+            isJumpKeyPressed = false;
         }
     }
 }
